e33c6d2ec12e5c1311ebfb2d49463f36981c50ca
Ruslan Sadykov <sadykovruslan91@gmail.com>
2021-09-07 01:42:44 +0300

    [CTR7,8]: Rebuilded users/me handler, converted app.js into functional component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import userThunkAC from "./redux/userThunkAC";
+import { useDispatch, useSelector } from "react-redux";
 
-class MainPage extends React.Component {
-  static propTypes = {
-    currentUser: PropTypes.object.isRequired,
+import { userFetch } from "./redux/userReducer";
+
+const App = () => {
+  const currentUser = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const sendUserRequest = () => {
+    dispatch(userFetch());
   };
 
-  sendUserRequest = () => {
-    this.props.dispatch(userThunkAC(this.props.dispatch));
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Redux check</h1>
-          <div>
-            <input type="button" value="Send" onClick={this.sendUserRequest} />
-            <span>Send users/me request</span>
-          </div>
-          <div>
-            <span>Status: {this.props.currentUser.status}</span>
-          </div>
-          {this.props.currentUser.data && (
-            <h3>Hello, {this.props.currentUser.data.firstName}</h3>
-          )}
-        </header>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = function (state) {
-  return {
-    currentUser: state.user,
-  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Redux check</h1>
+        <div>
+          <input type="button" value="Send" onClick={sendUserRequest} />
+          <span>Send users/me request</span>
+        </div>
+        <div>
+          <span>Status: {currentUser.status}</span>
+        </div>
+        {currentUser.data && <h3>Hello, {currentUser.data.firstName}</h3>}
+      </header>
+    </div>
+  );
 };
 
-export default connect(mapStateToProps)(MainPage);
+export default App;
--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -1,6 +1,25 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
 const TOKEN =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjNmMTdlNWMzY2IxNDVjMDQ1ZTk0ZSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYzMDk1NTk0NCwiZXhwIjoxNjMxODM0MzQ0fQ.AXc0KhpkkR9d475zpfQVH1nXugZ1SJomyQ1uJBSerRw";
+
+export const userFetch = createAsyncThunk(
+  "user/fetch",
+  async function (_, { rejectWithValue }) {
+    try {
+      const response = await fetch("http://localhost:3000/users/me", {
+        headers: { Authorization: TOKEN },
+      });
+      const responseJSON = await response.json();
+      if (!responseJSON.data) {
+        throw new Error(responseJSON.err.message);
+      }
+      return responseJSON;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
 
 const currentUserSlice = createSlice({
   name: "currentUser",
@@ -9,15 +28,16 @@ const currentUserSlice = createSlice({
     data: null,
     error: null,
   },
-  reducers: {
-    userError(state, action) {
+  extraReducers: {
+    [userFetch.fulfilled]: (state, action) => {
+      state.status = "success";
+      state.data = action.payload.data;
+      state.error = null;
+    },
+    [userFetch.rejected]: (state, action) => {
       state.status = "error";
       state.data = null;
-      console.log(action);
-    },
-    userLoaded(state, action) {
-      state.status = "success";
-      state.data = action.payload;
+      state.error = action.payload.err;
     },
   },
 });
